58ae3f8b5d9bb7ea6887b0354f924eecdbcf7e08
Marceli Wójciuk <marceliwojciuk@Marceli.local>
2020-02-21 00:34:30 +0100

    changes in server

--- server/src/main/java/com/example/demo/Controller/PlaceController.java
+++ server/src/main/java/com/example/demo/Controller/PlaceController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/place")
+@RequestMapping("/places")
 public class PlaceController {
 
 
